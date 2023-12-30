accdc06c8fd9ff2bb90d3beaf5cdccbcfe94efbe
pompolompo <enferrofa@gmail.com>
2023-12-30 15:44:06 +0100

    canvi d'idees, no cal posar l'output bonic.
    Cal considerar com descomptem l'influència en funció dels nivells de colindància i de distància (introducció de nou paràmetre talls en la variable distància).
    
    Després caldrà pensar en les incompatibilitats entre 'cabeceras', i.e es toquen.

--- algoritmo.R
+++ algoritmo.R
@@ -1,12 +1,14 @@
 library(terra)
 library(dplyr)
-library()
 
 malaga <- vect("data/shape_data/13_01_TerminoMunicipal.shp") %>%
   .[which(.[["provincia"]] == "Málaga"), ] # shapefile municipios málaga
 
 flujos <- read.csv("data/flujos.csv") # flujos municipales
 
+w = matrix(c(.1, .25, .25, .15, .1, .1 , .05))
+flujo = flujos %>% select(origen, destino, colind, dist) %>%
+  bind_cols(flux = as.matrix(flujos[,3:9]) %*% w)
 
 # selección cabeceras
 
@@ -14,27 +16,36 @@ flujos <- read.csv("data/flujos.csv") # flujos municipales
 k = 7 # número de cabeceras
 d = 10000 # distancia máxima a considerar
 c = 2 # grado de colindancia máxima a considerar
+i = c(-Inf, 1, 10, 20, Inf)*1000 # cortes niveles distancia
 
 # filtrar flujos
 aux = flujo %>% filter(dist <= d, colind <= c)
 
-# vull diferenciar la suma i mitjana segons el grau de colindancia,
-# de manera que es crein noves columnes com suma_1, suma_2, ..., suma_c y media_1, media_2, ..., media_c
-# ara mateix el resultat és una fila per cada combinació destí grau colindancia
-# puc intentar group by different o passar el resultat from long to wide
+# nivells distància
+aux$dist_l = cut(aux$dist, breaks = i, include.lowest = TRUE)
+        
+# cal pensar com descontem estar més lluny (en nivells de ditància o nivells de colindància)
+# així trobem una mesura única per ordenar els candidats a cabecera
+
+# després cal considerar les incompatibilitats (i.e dos candidats enganxats)
+
 
 # mesidas resumen
 resum0 = aux %>% group_by(destino) %>%
   summarise(
     n = n(),
     suma = sum(flux),
-    media = mean(flux)
+    media = mean(flux),
+    .groups = "keep"
   ) %>% arrange(desc(suma))
 
-# mesidas resumen
 resum1 = aux %>% group_by(destino, colind) %>%
   summarise(
     n = n(),
     suma = sum(flux),
-    media = mean(flux)
+    media = mean(flux),
+    .groups = "keep"
   ) %>% arrange(desc(suma))
+
+
+# 
