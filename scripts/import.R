860e3f280862638691937617c136991952a9e61b
pompolompo <enferrofa@gmail.com>
2024-01-13 20:26:19 +0100

    script para importar shapefile y datos de flujos

---
+++ scripts/import.R
@@ -0,0 +1,12 @@
+rm(list = ls())
+library(sf)
+library(readxl)
+
+### importar datos ###
+
+# shapefile
+shp = read_sf("datos/shape_data/13_01_TerminoMunicipal.shp") %>%
+  .[which(.[["provincia"]] == "Málaga"), ]
+
+# flujos municipales
+flux = read_xlsx("datos/malaga.xlsx")
