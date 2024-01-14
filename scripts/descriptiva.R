1b563700353332cb6cb944b7dc4508600fd1848a
pompolompo <enferrofa@gmail.com>
2024-01-14 14:41:15 +0100

    gráfico para los 8 municipios con mayor flujo entrante, explica:  - qué municipios dependen de él  - en qué medida dependen de él  - en qué medida dependen de cualquier municipio

--- scripts/descriptiva.R
+++ scripts/descriptiva.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(mapsf)
 source("funcs/codigo_nombre.R")
 ### tablas resumen ###
 
@@ -14,4 +15,69 @@ sumas = flux %>%
   filter(n >= 5) %>%
   head(8) %>%
   codigo_nombre(tbl = .)
+
+# gráfica con los municipios dependientes de los 8 anteriores
+dev.new()
+par(mfrow = c(4, 2))
+mf_theme("candy")
+
+for(m in sumas$DESTINO){
+  # códigos municipios dependientes
+  d = flux %>% filter(
+    DESTINO == codigo_nombre(m, nb_a_id = TRUE, num = TRUE)
+  ) %>% pull(ORIGEN)
   
+  # flujos hacia el municipio destino 
+  f = flux %>% filter(
+    DESTINO == codigo_nombre(m, nb_a_id = TRUE, num = TRUE)
+  ) %>% select(-DESTINO) %>%
+    mutate(FLUJO = rowSums(across('COMERCIO':'OTROS SERV')), .keep = "unused")
+  
+  # flujos hacia cualquier municipio
+  t = flux %>% 
+    filter(ORIGEN %in% d) %>% 
+    select(-DESTINO) %>%
+    group_by(ORIGEN) %>%
+    summarise(
+      TOTAL = sum(across(everything()))
+    )
+  
+  # shapefile con la información anterior
+  s = left_join(shp, f, join_by(cod_mun == ORIGEN)) %>%
+    left_join(t, join_by(cod_mun == ORIGEN))
+  
+  # municipios (no) dependientes y destino
+  s$municipio = NA
+  s$municipio[which(s$cod_mun %in% d)] = 0
+  s$municipio[which(s$nombre == m)] = 1
+  
+  # gráfico con dos variables
+  mf_map(x = s, var = "municipio", type = "typo", leg_pos = NA, 
+         pal = "Pastel 1", alpha = .7, col_na = "white")
+  
+  mf_map(s, var = c("FLUJO", "TOTAL") , type = "prop_choro", 
+         breaks = "geom", nbreaks = 5, pal = "Greens",
+         leg_val_rnd = c(0, -2), leg_pos = "topleft",
+         leg_frame = TRUE, inches = .1, add = TRUE)
+  
+  mf_layout(
+    title = paste("Municipios dependientes de", m),
+    credits = paste0(
+      "Fuente: Universidad de Sevilla\n",
+      "mapsf ",
+      packageVersion("mapsf")
+    ),
+    arrow = FALSE, frame = TRUE
+  )
+  mf_arrow(pos = "topright")
+  
+}
+
+### -----------------ZONA DE PRUEBAS----------------------- ###
+
+for(i in 1:30){
+  mf_get_pal(n = 10, pal = hcl.pals()[i]) %>%
+    plot(0:1, 0:1, bg = ., pch = 22, cex = 4, main = hcl.pals()[i])
+  Sys.sleep(1.5)
+}
+
