982eeb84d22d1435dc07d3dd8671d64b34009c6b
pompolompo <enferrofa@gmail.com>
2024-01-18 19:29:06 +0100

    scripts dónde se almacena el código de todas las tablas, separadas por secciones

---
+++ tabs/tab_script.R
@@ -0,0 +1,29 @@
+source("scripts/import.R")
+source("scripts/descriptiva.R") # -> sumas
+### METODOLOGÍA ###
+
+# 
+sumas %>% rename(
+  Destino = 'DESTINO',
+  tot = 'TOTAL',
+  com = 'COMERCIO',
+  educa = 'ENSEÑANZA',
+  salud = 'SANIDAD',
+  admin = 'ADMINISTRACIÓN',
+  ocio = 'OCIO Y CULTURA',
+  trans = 'TRANSPORTES',
+  otro = 'OTROS SERV'
+) %>% head(7) %>%
+  mutate(across('tot':'otro',
+                function(x) round(x/1000, 2))) %>% 
+  kable(booktabs = TRUE, format = "latex", 
+        caption = "Municipios con mayores flujos entrantes") %>% 
+  kable_styling(latex_options = c("striped", "hold_position")) %>%
+  footnote(general_title = "La tipología de los flujos es, de izquierda a derecha:",
+           general = "Total; Comercio; Educación; Sanidad; Administración; Ocio y Cultura; Transportes; Otros ",
+           footnote_as_chunk = FALSE, title_format = "underline") %>%
+  footnote(general_title = "Unidad de medida",
+           general = "Flujos en miles de pesonas",
+           footnote_as_chunk = TRUE, title_format = "underline") %>%
+  save_kable("tabs/top7.png")
+  
