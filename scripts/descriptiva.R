be2c39101e5ecf54153a6782c41099d848f364a3
pompolompo <enferrofa@gmail.com>
2024-01-18 19:28:23 +0100

    eliminamos el corte de los 8 primeros municipios
    se traspasa el gráfico al script de gráficos

--- scripts/descriptiva.R
+++ scripts/descriptiva.R
@@ -4,7 +4,7 @@ source("scripts/import.R")
 source("funcs/codigo_nombre.R")
 ### tablas resumen ###
 
-# 8 municipios con con almenos 5 dependientes de mayor flujo entrante
+# municipios con con almenos 5 dependientes de mayor flujo entrante
 sumas = flux %>%
   group_by(DESTINO) %>%
   summarise(
@@ -14,62 +14,4 @@ sumas = flux %>%
   arrange(desc(TOTAL)) %>%
   select(DESTINO, n, TOTAL, everything()) %>%
   filter(n >= 5) %>%
-  head(8) %>%
   codigo_nombre(tbl = .)
-
-# gráfica con los municipios dependientes de los 8 anteriores
-dev.new()
-par(mfrow = c(4, 2))
-mf_theme("candy")
-
-for(m in sumas$DESTINO){
-  # códigos municipios dependientes
-  d = flux %>% filter(
-    DESTINO == codigo_nombre(m, nb_a_id = TRUE, num = TRUE)
-  ) %>% pull(ORIGEN)
-  
-  # flujos hacia el municipio destino 
-  f = flux %>% filter(
-    DESTINO == codigo_nombre(m, nb_a_id = TRUE, num = TRUE)
-  ) %>% select(-DESTINO) %>%
-    mutate(FLUJO = rowSums(across('COMERCIO':'OTROS SERV')), .keep = "unused")
-  
-  # flujos hacia cualquier municipio
-  t = flux %>% 
-    filter(ORIGEN %in% d) %>% 
-    select(-DESTINO) %>%
-    group_by(ORIGEN) %>%
-    summarise(
-      TOTAL = sum(across(everything()))
-    )
-  
-  # shapefile con la información anterior
-  s = left_join(shp, f, join_by(cod_mun == ORIGEN)) %>%
-    left_join(t, join_by(cod_mun == ORIGEN))
-  
-  # municipios (no) dependientes y destino
-  s$municipio = NA
-  s$municipio[which(s$cod_mun %in% d)] = 0
-  s$municipio[which(s$nombre == m)] = 1
-  
-  # gráfico con dos variables
-  mf_map(x = s, var = "municipio", type = "typo", leg_pos = NA, 
-         pal = "Pastel 1", alpha = .7, col_na = "white")
-  
-  mf_map(s, var = c("FLUJO", "TOTAL") , type = "prop_choro", 
-         breaks = "geom", nbreaks = 5, pal = "Greens",
-         leg_val_rnd = c(0, -2), leg_pos = "topleft",
-         leg_frame = TRUE, inches = .1, add = TRUE)
-  
-  mf_layout(
-    title = paste("Municipios dependientes de", m),
-    credits = paste0(
-      "Fuente: Universidad de Sevilla\n",
-      "mapsf ",
-      packageVersion("mapsf")
-    ),
-    arrow = FALSE, frame = TRUE
-  )
-  mf_arrow(pos = "topright")
-  
-}
